5056be4b3ae91756111b0216f56caab479e253e6
Brecht Nys <123317735+NysB@users.noreply.github.com>
2023-07-31 22:06:00 -0400

    Commit Branch

--- app.py
+++ app.py
@@ -1,122 +1,102 @@
-# import libraris
+# import dependencies
 import pandas as pd
 
 import numpy as np
 from flask import Flask, render_template, request
 import pickle
 
-## col names for df
-col_names = ['Age', 
-            'Poverty_Ratio', 
-            'Gender_Female', 
-            'Gender_Male',
-            'Prediabetes_no', 
-            'Prediabetes_yes', 
-            'Categorical_BMI_Healthy_Weight',
-            'Categorical_BMI_Obese', 
-            'Categorical_BMI_Overweight',
-            'Categorical_BMI_Underweight', 
-            'Education_12th_Grade_no_diploma',
-            'Education_Associates_Academic_Program',
-            'Education_Associates_Occupational_Technical_Vocational',
-            'Education_Bachelor', 
-            'Education_GED_Equivalent',
-            'Education_Grade_1-11', 
-            'Education_Greater_Than_Master',
-            'Education_High_School_Graduate', 
-            'Education_Masters',
-            'Education_Some_College_no_degree', 
-            'Race_AIAN_AND_other',
-            'Race_AIAN_Only',
-            'Race_African_American_Only', 
-            'Race_Asian_Only',
-            'Race_Other',
-            'Race_White_Only']
-
+# col names for df
+input_fields = ["Gender",
+                "Married",
+                "Dependents",
+                "Education", 
+                "Self_Employed",
+                "ApplicantIncome",
+                "CoapplicantIncome",
+                "LoanAmount",
+                "Loan_Amount_Term",
+                "Credit_History",
+                "Property_Area"]
 
 # initialzie the flask app
 app = Flask(__name__)
 
-# load ml model
-model = pickle.load(open('model.pkl', 'rb'))
+# load logistic regression model
+model = pickle.load(open('logistic_regression.pkl', 'rb'))
 
-# define the app route for the default page of the web-app
+# define the app 
+
 @app.route('/')
 def home():
-    return render_template('index_manny.html')
+    return render_template('index.html')
 
 # to use the predict button in our web-app
 @app.route('/predict', methods=['POST'])
 def predict():
 
-    X = np.zeros( len(col_names) )
+    ## Input variables
+
+    X = np.zeros(len(input_fields))
     print("X",X)
-    df_XX = pd.DataFrame(data=[dict(zip(col_names, X) ) ] )
+    predictions_df = pd.DataFrame(data=[dict(zip(input_fields, X))])
+   
+    gender_input = request.form['Gender']
+    print("Gender:", gender_input)
+    predictions_df['Gender'] = gender_input
 
-    # for rending result on html GUI
+    married_input = request.form['Married']
+    print("Marital Status:", married_input)
+    predictions_df['Married'] = married_input
 
-    feat_prediabetic = f"Prediabetes_{request.form['prediabetes']}"
-    print( "Feat_prediabetic:", feat_prediabetic)
-    df_XX[ feat_prediabetic ] = 1.0
+    dependents_input = request.form['Dependents']
+    print("Dependents:", dependents_input)
+    predictions_df['Dependents'] = dependents_input
 
-    feat_weightlbs = int( request.form['weightlbs'] )
-    print( "Feat weightlbs:", feat_weightlbs)
+    education_input = request.form['Education']
+    print("Education:", education_input)
+    predictions_df['Education'] = education_input
 
-    feat_height = int ( request.form['height'] )
-    print( "Feat height:", feat_height )
+    self_employed_input = request.form['Self_Employed']
+    print("Self Employed:", self_employed_input)
+    predictions_df['Self_Employed'] = self_employed_input
 
-    bmi = 703 * ( feat_weightlbs / (feat_height * feat_height) )
-    print( "Feat bmi:", bmi )
+    applicant_income_input = request.form['ApplicantIncome']
+    print("Applicant Income:", applicant_income_input)
+    predictions_df['ApplicantIncome'] = applicant_income_input
 
-    if bmi <= 18.5:
-         df_XX['Categorical_BMI_Underweight'] = 1.0
-    elif bmi > 18.5 and bmi < 25:
-         df_XX['Categorical_BMI_Healthy_Weight'] = 1.0
-    elif bmi >= 25 and bmi < 30:
-         df_XX['Categorical_BMI_Overweight'] = 1.0
-    else:
-        df_XX['Categorical_BMI_Obese'] = 1.0
-    
-    feat_age = int( request.form[ 'age'])
-    print( "Feat age:", feat_age )
-    df_XX['Age'] = feat_age
+    co_applicant_income_input = request.form['CoapplicantIncome']
+    print("Co-Applicant Income:", co_applicant_income_input)
+    predictions_df['CoapplicantIncome'] = co_applicant_income_input
 
-    feat_gender = f"Gender_{request.form['gender']}"
-    print("Feat Gen:",feat_gender)
-    df_XX[ feat_gender ] = 1.0
+    loan_amount_input = request.form['LoanAmount']
+    print("Gender:", loan_amount_input)
+    predictions_df['LoanAmount'] = loan_amount_input
 
-    feat_education = f"Education_{request.form['education']}"
-    print("Feat Edu:",feat_education)
-    df_XX[ feat_education ] = 1.0
+    loan_amount_term_input = request.form['Loan_Amount_Term']
+    print("Loan Term:", loan_amount_term_input)
+    predictions_df['Loan_Amount_Term'] = loan_amount_term_input
 
-    feat_race = f"Race_{request.form['race']}"
-    print("Feat Race:", feat_race)
-    df_XX[ feat_race ] = 1.0
+    credit_history_input = request.form['Credit_History']
+    print("Credit History:", credit_history_input)
+    predictions_df['Credit_History'] = credit_history_input
 
-    feat_hhincome = float( request.form['income'] )
-    print( "Feat_hhincome:", feat_hhincome)
+    property_area_input = request.form['Property_Area']
+    print("Gender:", property_area_input)
+    predictions_df['Property_Area'] = property_area_input
 
-    feat_hhsize = int( request.form['size'] )
-    print( "Feat_hhsize:", feat_hhsize)
+    ## Make prediction
 
-    ratio = feat_hhincome / (15000 * feat_hhsize)
-    print( "Feat_ratio:", ratio)
-    df_XX[ 'Poverty_Ratio' ] = ratio
-
-    print( df_XX )
-    print( df_XX.columns )
-
-    prediction = model.predict_proba( df_XX )
+    prediction = model.predict_proba(predictions_df)
     print("prediction",prediction)
     
     output = np.round(prediction[0][1], 2)
 
-    print( 'You are likely: {}'.format(output) )
+    print('You are likely: {}'.format(output))
 
-    if output > (.65):
-        page = "sad.html"
+    if output > (.60):
+        page = "reject.html"
     else:
-        page = "happy.html"
+        page = "approve.html"
     return render_template(page, prediction_text='Probability: {}'.format(output))
 
 # start the flask server
